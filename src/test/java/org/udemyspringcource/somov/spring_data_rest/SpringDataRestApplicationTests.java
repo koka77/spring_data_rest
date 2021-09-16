28cb9869c2ba575cdf6f1800f75ea98c7b53cb6c
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2021-09-16 10:23:52 +0300

    проект с использованием spring-boot-starter-data-rest позволяет не писать контроллеры рест, подходит для простого проекта Так же добавлен актуатор, настрою в следующих коммитах

---
+++ src/test/java/org/udemyspringcource/somov/spring_data_rest/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udemyspringcource.somov.spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
